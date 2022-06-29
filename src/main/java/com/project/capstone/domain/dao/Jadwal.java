33baa12bbfc3a20075ce2e330a3d37d5c0b86f9e
azzahraga <azzahragalih021@gmail.com>
2022-06-29 14:52:44 +0700

    capstone

--- src/main/java/com/project/capstone/domain/dao/Jadwal.java
+++ src/main/java/com/project/capstone/domain/dao/Jadwal.java
@@ -13,6 +13,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
@@ -47,6 +48,7 @@ public class Jadwal extends BaseEntityWithDeletedAt{
     private String jp;
 
     @Column(name = "tanggal_kunjungan", nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date tanggal;
 
     @ManyToOne
