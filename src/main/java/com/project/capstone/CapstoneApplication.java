972e67a6e9022500854bda5af7d44ddb3688a649
azzahraga <azzahragalih021@gmail.com>
2022-07-04 13:53:49 +0700

    capstone

--- src/main/java/com/project/capstone/CapstoneApplication.java
+++ src/main/java/com/project/capstone/CapstoneApplication.java
@@ -1,7 +1,5 @@
 package com.project.capstone;
 
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
